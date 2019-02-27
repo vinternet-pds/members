105860d96e00c03489abfa6329f7f278f3d7126e
Archie Riley <litten15@gmail.com>
2019-02-27 15:55:02 +0000

    further updates

--- src/javascripts/overrides.js
+++ src/javascripts/overrides.js
@@ -1,3 +1,13 @@
 (function () {
   console.log('Override javascript executed!');
 })();
+
+// function hello() {
+//   console.log('Hello');
+// }
+
+function menu() {
+  console.log('test');
+  var x = document.getElementsByClassName('bigMenu');
+  x.style.display('block');
+}
